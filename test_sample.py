425d3643163f8fd95af5922e23c8d82bbf80032d
Andrii <andrii@upside-services.com>
2019-07-11 19:06:34 +0300

    add allure support

--- test_sample.py
+++ test_sample.py
@@ -1,7 +1,8 @@
-import pytest
-
+import allure
 
 class TestSelenium:
+
+    @allure.title("Sample")
     def test_first(self, driver):
         driver.get('https://github.com/')
         assert 'GitHub' in driver.title
